e6a597a45304d07a09df321bdc4f44c461adc46b
Hung Luong <hunglqhe161677@fpt.edu.vn>
2024-01-10 20:56:50 +0700

    Initialize project

---
+++ src/main/java/com/group3/ezquiz/EzquizApplication.java
@@ -0,0 +1,13 @@
+package com.group3.ezquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group3/ezquiz/EzquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.ezquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
