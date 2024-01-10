a2dea7284fcf3d12ea878feb5b77c7c22f6ca517
Hung Luong <hunglqhe161677@fpt.edu.vn>
2024-01-10 21:18:26 +0700

    Add model: Role

---
+++ src/main/java/com/group3/model/Role.java
@@ -0,0 +1,24 @@
+package com.group3.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "_role")
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String role;
+}
