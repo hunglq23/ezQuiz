007940abf0a582040c18bbc8664a01a1af069d68
Hung Luong <hunglqhe161677@fpt.edu.vn>
2024-01-14 11:48:28 +0700

    Add password encryption

--- src/main/java/com/group3/ezquiz/config/WebSecurityConfig.java
+++ src/main/java/com/group3/ezquiz/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,5 +29,10 @@ public class WebSecurityConfig {
 
         .formLogin(form -> form.loginPage("/login").permitAll());
     return http.build();
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
