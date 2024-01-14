c57b3d847154b5ab3490a020991d5885b50c312d
Hung Luong <hunglqhe161677@fpt.edu.vn>
2024-01-14 11:51:19 +0700

    Update model and payload

--- src/main/java/com/group3/ezquiz/model/User.java
+++ src/main/java/com/group3/ezquiz/model/User.java
@@ -10,8 +10,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
