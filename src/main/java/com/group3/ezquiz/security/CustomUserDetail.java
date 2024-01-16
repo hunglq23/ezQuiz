e2f6dd0199901baa38841c3532ff33d304586003
TranDucThinhK17HL <thinhtdhe173229@fpt.edu.vn>
2024-01-16 20:56:20 +0700

    check smt

--- src/main/java/com/group3/ezquiz/security/CustomUserDetail.java
+++ src/main/java/com/group3/ezquiz/security/CustomUserDetail.java
@@ -3,9 +3,7 @@ package com.group3.ezquiz.security;
 import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
