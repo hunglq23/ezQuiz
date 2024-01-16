5872ffa65da18f622ecc02e26dfd0c33dc36926e
Hung Luong <hunglqhe161677@fpt.edu.vn>
2024-01-16 18:24:52 +0700

    Add User Controller for Common Feature

--- src/main/java/com/group3/ezquiz/controller/UserController.java
+++ src/main/java/com/group3/ezquiz/controller/UserController.java
@@ -1,13 +1,21 @@
 package com.group3.ezquiz.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LearnerController {
+@PreAuthorize("hasAnyRole('LEARNER','TEACHER','ADMIN')")
+public class UserController {
+
   @GetMapping("/home")
   public String getLearnerHomepage() {
     return "home";
+  }
+
+  @GetMapping("/profile")
+  public String getProfile() {
+    return "profile";
   }
 
 }
