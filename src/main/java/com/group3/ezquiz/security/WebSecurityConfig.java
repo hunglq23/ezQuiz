f1612113fd9763ceac0845abf7ab9e473aee6631
Hung Luong <hunglqhe161677@fpt.edu.vn>
2024-01-21 11:33:07 +0700

    Update config - refactor

--- src/main/java/com/group3/ezquiz/security/WebSecurityConfig.java
+++ src/main/java/com/group3/ezquiz/security/WebSecurityConfig.java
@@ -1,25 +1,15 @@
 package com.group3.ezquiz.security;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -38,12 +28,6 @@ import lombok.RequiredArgsConstructor;
 public class WebSecurityConfig {
 
   private final CustomOAuth2UserService oAuth2UserService;
-  // private UserService userService;
-
-  // @Autowired
-  // public void setUserService(UserService userService) {
-  // this.userService = userService;
-  // }
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -77,12 +61,9 @@ public class WebSecurityConfig {
                       HttpServletRequest request,
                       HttpServletResponse response,
                       Authentication authentication) throws IOException, ServletException {
-
                     // CustomOAuth2User oauthUser = (CustomOAuth2User)
                     // authentication.getPrincipal();
-
                     // userService.processOAuthPostLogin(oauthUser.getEmail());
-
                     response.sendRedirect("/profile");
                   }
                 }))
@@ -106,33 +87,6 @@ public class WebSecurityConfig {
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
-  }
-
-  private GrantedAuthoritiesMapper grantedAuthoritiesMapper() {
-    return (authorities) -> {
-      Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
-
-      authorities.forEach((authority) -> {
-        GrantedAuthority mappedAuthority;
-
-        if (authority instanceof OidcUserAuthority) {
-          OidcUserAuthority userAuthority = (OidcUserAuthority) authority;
-          OidcUserInfo userInfo = userAuthority.getUserInfo();
-          mappedAuthority = new OidcUserAuthority(
-              "OIDC_USER", userAuthority.getIdToken(), userInfo);
-        } else if (authority instanceof OAuth2UserAuthority) {
-          OAuth2UserAuthority userAuthority = (OAuth2UserAuthority) authority;
-          mappedAuthority = new OAuth2UserAuthority(
-              "OAUTH2_USER", userAuthority.getAttributes());
-        } else {
-          mappedAuthority = authority;
-        }
-
-        mappedAuthorities.add(mappedAuthority);
-      });
-
-      return mappedAuthorities;
-    };
   }
 
 }
