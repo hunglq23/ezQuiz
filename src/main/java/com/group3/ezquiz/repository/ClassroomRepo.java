5c33de7107b8945f0717a04c3a2ec7c277d9986e
Ngocne21 <NgocPTBHE176778@fpt.edu.vn>
2024-01-30 14:17:12 +0700

    Add pagination in repo classroom

--- src/main/java/com/group3/ezquiz/repository/ClassroomRepo.java
+++ src/main/java/com/group3/ezquiz/repository/ClassroomRepo.java
@@ -2,6 +2,7 @@ package com.group3.ezquiz.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +10,9 @@ import org.springframework.data.domain.Pageable;
 import com.group3.ezquiz.model.Classroom;
 
 @Repository
-public interface ClassroomRepo extends JpaRepository<Classroom, Long>{
-    @Query("Select c from Classroom c where c.className like  %?1%")
-    public Page<Classroom> findAll(String keyword, Pageable pageable);
+public interface ClassroomRepo extends JpaRepository<Classroom, Long> {
+        @Query ("Select c from Classroom c where" + 
+        "(:sName is null or c.className Like %:sName%)")
+
+        Page<Classroom> getAllClassroom(String sName, Pageable page);
 }
