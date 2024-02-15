fa42bb59255dd4a83dca0434ea27909f9f97ee72
Hung Luong <hunglqhe161677@fpt.edu.vn>
2024-02-16 02:40:21 +0700

    Add bind exception handler and error detail

--- src/main/java/com/group3/ezquiz/exception/GlobalExceptionHandler.java
+++ src/main/java/com/group3/ezquiz/exception/GlobalExceptionHandler.java
@@ -1,7 +1,15 @@
 package com.group3.ezquiz.exception;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -9,6 +17,26 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(ResourceNotFoundException.class)
   public String handleResourceNotFoundException() {
     return "error/404";
+  }
+
+  @ExceptionHandler(BindException.class)
+  public ResponseEntity<ErrorDetails> handleBindException(
+      BindException exception,
+      WebRequest webRequest) {
+    Map<String, String> errors = new HashMap<>();
+    exception.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField() + "Error";
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+
+    return ResponseEntity.badRequest().body(
+        ErrorDetails.builder()
+            .timestamp(LocalDateTime.now())
+            .message(exception.getMessage())
+            .details(webRequest.getDescription(false))
+            .errors(errors)
+            .build());
   }
 
 }
