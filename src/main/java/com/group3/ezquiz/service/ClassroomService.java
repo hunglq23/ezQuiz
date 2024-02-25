406fbb309fcb7b59cc5ac01784a4e5c19838c47a
Ngocne21 <NgocPTBHE176778@fpt.edu.vn>
2024-02-25 23:12:25 +0700

    add feature in classroomService

--- src/main/java/com/group3/ezquiz/service/ClassroomService.java
+++ src/main/java/com/group3/ezquiz/service/ClassroomService.java
@@ -4,19 +4,20 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import com.group3.ezquiz.model.Classroom;
+import com.group3.ezquiz.model.User;
 import com.group3.ezquiz.payload.ClassroomDto;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface ClassroomService {
     void createClass(HttpServletRequest request, ClassroomDto classroomDto);
 
-    Optional<Classroom> getClassroomById(Long id);
+    Optional<Classroom> getClassroomByRequestAndId(HttpServletRequest request, Long id);
 
     Classroom updateClassroom(Long id, Classroom updatedClassroom);
 
     void deleteClassroomById(Long id);
 
-    Page<Classroom> getClassListByPageAndSearchName(Integer page, String searchName);
+    Page<Classroom> getCreatedClassListByPageAndSearchName(HttpServletRequest request, Integer page, String searchName);
 
     Classroom findByCode(String code);
 
@@ -24,5 +25,14 @@ public interface ClassroomService {
 
     boolean joinClassroom(HttpServletRequest request, String code);
 
-    List<Classroom> getAllClassroom();
+    User getUserRequesting(HttpServletRequest request);
+
+    void removeMemberFromClassroom(Long classId, Long memberId, User requestingUser);
+
+   List <Classroom> getCreatedClassrooms(HttpServletRequest request);
+
+Classroom removeMemberFromClassroomByMemberId(Classroom classroom, Long memberId);
+
+    
+
 }
