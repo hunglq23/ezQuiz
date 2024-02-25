62815c242d4f1984312a3d661a2c1d46e5f25724
Ngocne21 <NgocPTBHE176778@fpt.edu.vn>
2024-02-25 23:06:40 +0700

    Add feature for classroom

--- src/main/java/com/group3/ezquiz/controller/ClassroomController.java
+++ src/main/java/com/group3/ezquiz/controller/ClassroomController.java
@@ -1,6 +1,7 @@
 package com.group3.ezquiz.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.group3.ezquiz.exception.ResourceNotFoundException;
 import com.group3.ezquiz.model.Classroom;
 import com.group3.ezquiz.payload.ClassroomDto;
 import com.group3.ezquiz.payload.CodeFormDto;
@@ -19,6 +23,7 @@ import com.group3.ezquiz.service.ClassroomService;
 import com.group3.ezquiz.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
@@ -31,33 +36,47 @@ public class ClassroomController {
     @Autowired
     private UserService userService;
 
+    // @PreAuthorize("hasRole('ROLE_TEACHER')")
+    // @GetMapping("/classrooms/created")
+    // public String getCreatedClassrooms(HttpServletRequest request, Model model,
+    //         @RequestParam(defaultValue = "0") Integer page,
+    //         @RequestParam(required = false, defaultValue = "") String name) {
+    //     Page<Classroom> classrooms = classroomService.getCreatedClassListByPageAndSearchName(request, page, name);
+
+    //     model.addAttribute("classrooms", classrooms.getContent());
+    //     model.addAttribute("currentPage", page);
+    //     model.addAttribute("totalPages", classrooms.getTotalPages());
+    //     model.addAttribute("search", name);
+    //     model.addAttribute("classroom", new ClassroomDto());
+    //     return "classroom/class-list";
+    // }
+
     @PreAuthorize("hasRole('ROLE_TEACHER')")
-    @GetMapping("/classrooms/created")
-    public String getCreatedClassrooms(Model model,
-            @RequestParam(defaultValue = "0") Integer page,
-            @RequestParam(required = false, defaultValue = "") String name) {
-        Page<Classroom> classrooms = classroomService.getClassListByPageAndSearchName(page, name);
-        model.addAttribute("classrooms", classrooms.getContent());
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", classrooms.getTotalPages());
-        model.addAttribute("search", name);
+    @GetMapping("/classrooms/created-list")
+    public String getCreatedClassrooms(HttpServletRequest request, Model model) {
+        List<Classroom> classrooms = classroomService.getCreatedClassrooms(request);
+        model.addAttribute("classrooms", classrooms);
         model.addAttribute("classroom", new ClassroomDto());
-        return "classroom/class-list";
+        return "classroom/created-list";
     }
 
     @PreAuthorize("hasRole('ROLE_TEACHER')")
-    @PostMapping("/classrooms/created")
-    public String ClassCreating(HttpServletRequest hRequest,
+    @PostMapping("/classrooms/create")
+    public String ClassCreating( HttpServletRequest hRequest,
             @ModelAttribute("classroom") ClassroomDto classroomDto) {
         classroomService.createClass(hRequest, classroomDto);
-        return "redirect:/classrooms/created"; // den dia chi
+        return "redirect:/classrooms/created-list"; // den dia chi
     }
 
-    @GetMapping("/classroom/update/{id}")
-    public String getClassroomDetailandUpdate(@PathVariable(value = "id") Long id, Model model) {
-        Classroom classroom = classroomService.getClassroomById(id)
-                .orElseThrow(() -> new RuntimeException("Classroom not be found id " + id));
+    @GetMapping("/classroom/{id}")
+    public String getClassroomDetail(
+            HttpServletRequest request,
+            @PathVariable(value = "id") Long id,
+            String code, Model model) {
+        Classroom classroom = classroomService.getClassroomByRequestAndId(request, id)
+                .orElseThrow(() -> new ResourceNotFoundException("Classroom not be found id " + id));
         ;
+
         model.addAttribute(("classroom"), classroom);
         return "classroom/classroom-update";
     }
@@ -73,13 +92,14 @@ public class ClassroomController {
     @GetMapping("/classroom/delete/{id}")
     public String ClassroomDeleting(@PathVariable(value = "id") Long id) {
         classroomService.deleteClassroomById(id);
-        return "redirect:/classrooms/created";
+        return "redirect:/classrooms/created-list";
     }
 
     @PreAuthorize("hasRole('ROLE_LEARNER')")
     @GetMapping("/classroom/joined-list")
-    public String joinClassroomForm(Model model) {
+    public String joinClassroomForm(HttpServletRequest request, Model model) {
         model.addAttribute("classroom", new CodeFormDto());
+        model.addAttribute("classrooms", userService.getUserRequesting(request).getClassrooms());
         return "classroom/joined-list";
     }
 
@@ -94,5 +114,16 @@ public class ClassroomController {
         }
         return "error";
     }
-}
 
+    @GetMapping("/classroom/{id}/remove-member")
+    public String removeMemberByTeacher(
+            HttpServletRequest request,
+            @PathVariable(value = "id") Long id,
+            @RequestParam(required = true) Long memberId, Model model) {
+        Classroom classroom = classroomService.getClassroomByRequestAndId(request, id)
+                .orElseThrow(() -> new ResourceNotFoundException("Classroom not be found id " + id));
+        model.addAttribute("classroom", classroomService.removeMemberFromClassroomByMemberId(classroom, memberId));
+        return "classroom/classroom-update";
+    }
+
+}
