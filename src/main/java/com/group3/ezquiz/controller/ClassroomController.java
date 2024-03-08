8da0c09fca350fe176f53853ff076341faf713e4
Ngocne21 <NgocPTBHE176778@fpt.edu.vn>
2024-03-09 05:14:05 +0700

    Add methods in classroom

--- src/main/java/com/group3/ezquiz/controller/ClassroomController.java
+++ src/main/java/com/group3/ezquiz/controller/ClassroomController.java
@@ -1,78 +1,121 @@
 package com.group3.ezquiz.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.group3.ezquiz.model.Classroom;
 import com.group3.ezquiz.payload.ClassroomDto;
-import com.group3.ezquiz.service.ClassroomService;
+import com.group3.ezquiz.payload.CodeFormDto;
+import com.group3.ezquiz.service.IClassroomService;
+import com.group3.ezquiz.service.IUserService;
 import jakarta.servlet.http.HttpServletRequest;
-
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
+@RequiredArgsConstructor
+@RequestMapping("/classroom")
 public class ClassroomController {
-    @Autowired
-    private ClassroomService classroomService;
 
-    @PreAuthorize("hasRole('ROLE_TEACHER')")
-    @GetMapping("/classrooms/created")
-    public String getCreatedClassrooms(Model model,
-            @RequestParam(defaultValue = "0") Integer page,
-            @RequestParam(required = false, defaultValue = "") String name) {
-        Page<Classroom> classrooms = classroomService.getClassListByPageAndSearchName(page, name);
-        model.addAttribute("classroom", classrooms.getContent());
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", classrooms.getTotalPages());
-        model.addAttribute("search", name);
-        return "classroom/class-list";
+    private final String TEACHER_AUTHORITY = "hasRole('ROLE_TEACHER')";
+    private final String LEARNER_AUTHORITY = "hasRole('ROLE_LEARNER')";
+
+    private final IClassroomService classroomService;
+
+    private final IUserService userService;
+
+    @PreAuthorize(TEACHER_AUTHORITY)
+    @GetMapping("/created-list")
+    public String getCreatedClassrooms(HttpServletRequest request, Model model) {
+        List<Classroom> classrooms = classroomService.getCreatedClassrooms(request);
+        model.addAttribute("classrooms", classrooms);
+        return "classroom/lib-classroom";
     }
 
-    @PreAuthorize("hasRole('ROLE_TEACHER')")
-    @GetMapping("/classroom/create")
-    public String getClassCreatingForm(Model model) {
-        model.addAttribute("classroom", new ClassroomDto());
-        return "classroom/classroom-creating"; // den file html
+    @PreAuthorize(TEACHER_AUTHORITY)
+    @PostMapping("/create")
+    public ResponseEntity<?> ClassCreating(
+            HttpServletRequest request,
+            @Valid ClassroomDto classroomDto) throws BindException {
+
+        return classroomService.createClass(request, classroomDto); // den dia chi
     }
 
-    @PreAuthorize("hasRole('ROLE_TEACHER')")
-    @PostMapping("/classroom/create")
-    public String ClassCreating(HttpServletRequest hRequest,
-            @ModelAttribute("classroom") ClassroomDto classroomDto) {
-        classroomService.createClass(hRequest, classroomDto);
-        return "redirect:/classrooms/created"; // den dia chi
+    @PreAuthorize(TEACHER_AUTHORITY)
+    @GetMapping("/{id}")
+    public String getClassDetail(
+            HttpServletRequest request,
+            @PathVariable(value = "id") Long id,
+            String code, Model model) {
+        Classroom classroom = classroomService.getClassroomByRequestAndId(request, id);
+        model.addAttribute("classroom", classroom);
+
+        return "classroom/classroom-update";
     }
 
-    @GetMapping("/classroom/update/{id}")
-    public String getClassroomCreatingForm(@PathVariable(value = "id") Long id, Model model) {
-        Classroom classroom = classroomService.getClassroomById(id)
-                .orElseThrow(() -> new RuntimeException("Classroom not be found id " + id));
-        ;
-        model.addAttribute(("classroom"), classroom);
-        return "/classroom/classroom-updating";
-    }
-
-    @PostMapping("/classroom/update/{id}")
-
-    public String ClassroomUpdating(@PathVariable(value = "id") Long id,
-            @ModelAttribute("classroom") Classroom updatedClassroom) {
+    @PostMapping("/{id}/update")
+    public String ClassroomUpdating(
+            @PathVariable(value = "id") Long id,
+            @ModelAttribute("classroom") Classroom updatedClassroom,
+            RedirectAttributes redirectAttributes) {
         classroomService.updateClassroom(id, updatedClassroom);
-        return "redirect:/classrooms/created";
+        redirectAttributes.addFlashAttribute("successMessage", "Classroom updated successfully!");
+        return "redirect:/classroom/" + id;
 
     }
 
-    @GetMapping("/classroom/delete/{id}")
-    public String ClassroomDeleting(@PathVariable(value = "id") Long id) {
-        classroomService.deleteClassroomById(id);
-        return "redirect:/classrooms/created";
+    @PreAuthorize(TEACHER_AUTHORITY)
+    @GetMapping("/{id}/delete")
+    public String ClassDeleting(
+            @PathVariable(value = "id") Long id) {
+        classroomService.deleteClassById(id);
+        return "redirect:/classroom/created-list";
     }
+
+    @PreAuthorize(LEARNER_AUTHORITY)
+    @GetMapping("/joined-list")
+    public String joinClassroomForm(HttpServletRequest request, Model model) {
+        model.addAttribute("classroom", new CodeFormDto());
+        model.addAttribute("classrooms", userService.getUserRequesting(request).getClassrooms());
+        return "classroom/joined-list";
+    }
+
+    @PreAuthorize(LEARNER_AUTHORITY)
+    @PostMapping("/join")
+    public String joinClassroom(
+            HttpServletRequest request,
+            @RequestParam String code, Model model) {
+        boolean success = classroomService.joinClassroom(request, code);
+        if (success) {
+            model.addAttribute("classrooms", userService.getUserRequesting(request).getClassrooms());
+            return "classroom/joined-list";
+        }
+        return "error";
+    }
+
+    @PreAuthorize(TEACHER_AUTHORITY)
+    @GetMapping("/{id}/remove-member")
+    public String removeMemberByTeacher(
+            HttpServletRequest request,
+            @PathVariable(value = "id") Long id,
+            @RequestParam(required = true) Long memberId,
+            Model model) {
+                Classroom classroom = classroomService.getClassroomByRequestAndId(request, id);
+                model.addAttribute("classroom", classroomService.removeMemberFromClassroomByMemberId(classroom,memberId));
+        return "classroom/classroom-update";
+    }
+  
 
 }
+//
