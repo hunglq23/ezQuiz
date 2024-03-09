9ede9e9c24c3f29b9ee1e9ca1df96aeb1bc5be8c
pnseaa <nguyentcphe176116@fpt.edu.vn>
2024-03-09 19:28:50 +0700

    move nguyen2 to main-draft-nguyen

--- src/main/java/com/group3/ezquiz/controller/UserController.java
+++ src/main/java/com/group3/ezquiz/controller/UserController.java
@@ -1,20 +1,20 @@
 package com.group3.ezquiz.controller;
 
 import com.group3.ezquiz.payload.UserDto;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.group3.ezquiz.model.User;
 import com.group3.ezquiz.service.IUserService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Objects;
 
@@ -23,6 +23,7 @@ import java.util.Objects;
 public class UserController {
 
   private final IUserService userService;
+  private final PasswordEncoder passwordEncoder;
 
   @GetMapping("/home")
   public String getHomePage(HttpServletRequest http, Model model) {
@@ -38,7 +39,7 @@ public class UserController {
       @RequestParam(required = false, defaultValue = "", name = "email") String email,
       @RequestParam(required = false, defaultValue = "all", name = "status") String statusReq) {
     Boolean status = Objects.equals(statusReq, "all") ? null : Boolean.valueOf(statusReq);
-    Page<User> userList = userService.getListUser(http, email, status, PageRequest.of(page, 10));
+    Page<User> userList = userService.getListUser(http, email, status, PageRequest.of(page, 2));
     model.addAttribute("userList", userList);
     model.addAttribute("items", userList.getContent());
     model.addAttribute("currentPage", page);
@@ -82,5 +83,30 @@ public class UserController {
       @PathVariable(name = "id") Long id) {
     userService.delete(id);
     return "redirect:/admin/list";
+  }
+
+  @GetMapping("/profile")
+  public String getProfilePage(HttpServletRequest http, Model model) {
+    User userRequesting = userService.getUserRequesting(http);
+    model.addAttribute("user", userRequesting);
+    return "profile";
+  }
+
+  @PostMapping ("/user/update-password")
+  public String updatePass( HttpServletRequest http, Model model,
+                            @Valid @ModelAttribute("oldPass") String oldPass,
+                            @Valid @ModelAttribute("newPass") String newPass,
+                            @Valid @ModelAttribute("reNewPass") String reNewPass, RedirectAttributes redirectAttributes) {
+    User userRequesting = userService.getUserRequesting(http);
+    boolean isPasswordCorrect = passwordEncoder.matches(oldPass, userRequesting.getPassword());
+    model.addAttribute("user", userRequesting);
+    String successMessage = "updated fail!";
+    if(isPasswordCorrect && newPass.equals(reNewPass)) {
+      userService.updatePassword(userRequesting.getEmail(), newPass);
+      model.addAttribute("message", "success");
+      successMessage = "updated successfully!";
+    }
+    redirectAttributes.addFlashAttribute("successMessage", successMessage);
+    return "redirect:/profile";
   }
 }
---
+++ src/main/java/com/group3/ezquiz/service/EmailService.java
@@ -0,0 +1,25 @@
+package com.group3.ezquiz.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendSimpleMessage(String to, String subject, String text) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, StandardCharsets.UTF_8.name());
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+        javaMailSender.send(message);
+    }
+}
