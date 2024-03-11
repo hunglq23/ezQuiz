1e5ae6e45935351f033b2b5c5799eba6da0cfe1b
Hung Luong <hunglqhe161677@fpt.edu.vn>
2024-03-11 10:43:32 +0700

    Update readme ver1

--- src/main/java/com/group3/ezquiz/controller/UserController.java
+++ src/main/java/com/group3/ezquiz/controller/UserController.java
@@ -1,8 +1,5 @@
 package com.group3.ezquiz.controller;
 
-import jakarta.validation.Valid;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,14 +9,12 @@ import com.group3.ezquiz.service.IUserService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
   private final IUserService userService;
-  private final PasswordEncoder passwordEncoder;
 
   @GetMapping("/home")
   public String getHomePage(HttpServletRequest http, Model model) {
@@ -29,28 +24,4 @@ public class UserController {
     return "home";
   }
 
-  @GetMapping("/change-password")
-  public String getProfilePage(HttpServletRequest http, Model model) {
-    User userRequesting = userService.getUserRequesting(http);
-    model.addAttribute("user", userRequesting);
-    return "password-change";
-  }
-
-  @PostMapping("/update-password")
-  public String updatePass(HttpServletRequest http, Model model,
-      @Valid @ModelAttribute("oldPass") String oldPass,
-      @Valid @ModelAttribute("newPass") String newPass,
-      @Valid @ModelAttribute("reNewPass") String reNewPass, RedirectAttributes redirectAttributes) {
-    User userRequesting = userService.getUserRequesting(http);
-    boolean isPasswordCorrect = passwordEncoder.matches(oldPass, userRequesting.getPassword());
-    model.addAttribute("user", userRequesting);
-    String successMessage = "updated fail!";
-    if (isPasswordCorrect && newPass.equals(reNewPass)) {
-      userService.updatePassword(userRequesting.getEmail(), newPass);
-      model.addAttribute("message", "success");
-      successMessage = "updated successfully!";
-    }
-    redirectAttributes.addFlashAttribute("successMessage", successMessage);
-    return "redirect:/change-password";
-  }
 }
