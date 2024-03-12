3176bd051cfd943db3a1b2dc97774845516bd006
Ngocne21 <NgocPTBHE176778@fpt.edu.vn>
2024-03-13 01:54:20 +0700

    Update feature classroom

--- src/main/java/com/group3/ezquiz/repository/ClassroomRepo.java
+++ src/main/java/com/group3/ezquiz/repository/ClassroomRepo.java
@@ -1,11 +1,11 @@
 package com.group3.ezquiz.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.group3.ezquiz.model.Classroom;
 import com.group3.ezquiz.model.User;
 import java.util.List;
 import java.util.Optional;
-
 
 @Repository
 public interface ClassroomRepo extends JpaRepository<Classroom, Long> {
@@ -14,4 +14,8 @@ public interface ClassroomRepo extends JpaRepository<Classroom, Long> {
         Optional<Classroom> findByCreatorAndName(User creator, String name);
 
         Optional<Classroom> findByIdAndCreator(Long id, User creator);
+
+        Classroom findClassById(Long id);
+
+        Classroom findByCode(String code);
 }
